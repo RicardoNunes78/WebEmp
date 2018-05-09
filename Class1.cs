05750b77221646a4bb16b9ef5ce47f50c38b268f
Ricardo Nunes <is.ricardon@gmail.com>
2018-05-09 19:29:40 -0300

    Primeiro Commit

---
+++ Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace MVC_BO.Models
+{
+	public class Pessoa
+	{
+        public int PessoaID { get; set; };
+        public string Nome { get; set; };
+        public string Sexo { get; set; };
+        public string Email { get; set; };
+        public string Nome { get; set; };
+        public DateTime Nacimento { get; set; };
+    }
+}
